ee3e5fca0840bb446653650547c333bbf71133fd
Ripto <ripto25@gmail.com>
2022-02-12 19:08:31 +0700

    Tambah Data

---
+++ app/Http/Controllers/Admin/TamuController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class TamuController extends Controller
+{
+    public function index(){
+        $data = User::all();
+
+        return view('Admin.Tamu.index', compact('data'));
+    }
+
+    public function formTambah(){
+        return view('Admin.Tamu.formTambah');
+    }
+
+    public function simpanData(Request $request){
+        $nama    = $request->nama;
+        $telepon = $request->telepon;
+        $alamat  = $request->alamat;
+        $email   = $request->email;
+
+        $data = new User;
+        $data->nama =  $nama;
+        $data->tlp  =  $telepon;
+        $data->alamat = $alamat;
+        $data->email = $email;
+        $data->password = Hash::make('rahasia');
+
+        $data->save();
+
+        return redirect('admin/tamu')->with('status', 'Data Berhasil Disimpan');
+
+        
+    }
+
+}
---
+++ resources/views/Admin/Tamu/index.blade.php
@@ -0,0 +1,51 @@
+@extends('app')
+@section('content')
+    <div class="card">
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+        <div class="card-header">
+        Data Buku Tamu  <a href="{{url('admin/form-tambah')}}" class="btn btn-success"> Tambah Data</a>
+        </div>
+        <div class="card-body">
+            <table class="table" id="myTable">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Telepon</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach($data as $key => $item)
+                        <tr>
+                            <th scope="row">{{$key+ 1}}</th>
+                            <td>{{ $item->nama}}</td>
+                            <td>{{ $item->tlp}}</td>
+                            <td>{{ $item->alamat}}</td>
+                            <td>{{ $item->email}}</td>
+                            <td>
+                                <a href="" class="btn btn-warning"> Edit</a>
+                                <a href="" class="btn btn-danger"> Hapus</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        } );
+    </script>
+@endsection
+

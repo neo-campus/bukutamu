85f2ffe78de866fae536bdb1f686aecaab42b00d
Ripto <ripto25@gmail.com>
2022-02-12 19:01:14 +0700

    tambah data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\TamuController as AdminTamuController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\User\TamuController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,10 @@ Route::get('/', function () {
 
 Route::post('simpan-bukutamu', [TamuController::class, 'simpanTamu'])->name('simpan-bukutamu');
 
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+// Bagian Admin
+Route::get('admin/tamu', [AdminTamuController::class, 'index'])->name('admin-tamu');
+Route::get('admin/form-tambah', [AdminTamuController::class, 'formTambah'])->name('admin-form-tambah');
+Route::post('admin/simpan-data', [AdminTamuController::class, 'simpanData'])->name('admin-simpan-data');
